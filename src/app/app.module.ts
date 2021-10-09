4e1f73787ec91cf0a42075dbf6ae11039633dcf5
joulucas <joubertvh1@hotmail.com>
2021-10-08 23:30:09 -0300

    ajustado telas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
-
-import {MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { DadosComponent } from './views/dados/dados.component';
 
-import { MatSidenavModule } from  '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    DadosComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +32,11 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
+    RouterModule.forRoot([
+      {path: 'dados', component: DadosComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
